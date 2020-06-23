f1d43eacf7d7fcf1ffe70da33fc6a4f3c9832f5f
brunchz <info@brunchz.com>
2020-06-23 21:57:33 +0800

    changed url for 'create payment intent'

--- store/index.js
+++ store/index.js
@@ -69,7 +69,7 @@ export const actions = {
     try {
       // Create a PaymentIntent with the information about the order
       const result = await axios.post(
-        "https://ecommerce-netlify.netlify.app/.netlify/functions/create-payment-intent",
+        "https://inspiring-darwin-006640.netlify.app/.netlify/functions/create-payment-intent",
         {
           items: getters.cartItems
         },
